b8db69c33f6c6db7be490549e88b5f1966a7f55d
9onin <98697092+9onin@users.noreply.github.com>
2024-05-22 19:37:56 +0300

    Add files via upload

---
+++ Code of mine mission.main.py
@@ -0,0 +1,50 @@
+# Установка Flask:
+pip install Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Добро пожаловать в информационную систему учета и отслеживания доставки товаров!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+import sqlite3
+
+conn = sqlite3.connect('delivery.db')
+c = conn.cursor()
+
+# Создание таблицы для хранения заказов
+c.execute('''CREATE TABLE orders
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              order_number TEXT,
+              status TEXT,
+              estimated_delivery_date TEXT)''')
+
+# Внесение тестовых данных
+c.execute("INSERT INTO orders (order_number, status, estimated_delivery_date) VALUES ('1', 'В пути', '2022-10-10')")
+c.execute("INSERT INTO orders (order_number, status, estimated_delivery_date) VALUES ('2', 'Доставлено', '2022-09-30')")
+
+conn.commit()
+conn.close()
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/orders')
+def get_orders():
+    conn = sqlite3.connect('delivery.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM orders")
+    
+    orders = c.fetchall()
+
+    conn.close()
+    
+    return render_template('orders.html', orders=orders)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
